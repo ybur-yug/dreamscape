57e703ede14effb70d70eb5ef2e3eeceb0d0c9dd
bobby grayson <bobbygrayson@gmail.com>
2015-07-05 15:01:45 -0400

    setup scripts

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get install python3.4-dev
+sudo apt-get install python-pip
+sudo apt-get install python-numpy
+sudo apt-get install python-scipy
+sudo apt-get build-dep python-imaging
+sudo apt-get install libjpeg8 libjpeg62-dev libfreetype6 libfreetype6-dev
+sudo pip install Pillow
+pip install ipython
